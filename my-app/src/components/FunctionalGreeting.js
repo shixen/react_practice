36b6a44ed697682e613bcb062ce4a943e55415f0
Niklas Eriksson <niklaseriksson32@hotmail.com>
2024-06-23 11:51:12 +0000

    create function to return html

---
+++ my-app/src/components/FunctionalGreeting.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+// function FunctionalGreeing(){
+//     return <h1>Hello from react</h1>
+// }
+
+const FunctionalGreeing = () => <h1> Hello from React!!! </h1>
+
+export default FunctionalGreeing;
